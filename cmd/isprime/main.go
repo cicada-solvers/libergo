6e83af6041ceee9302a0eb06b097357b41558b6a
cmbsolver <cmbsolver@gmail.com>
2025-01-19 22:44:59 -0600

    Adding the prime checker.

---
+++ cmd/isprime/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"math/big"
+	"os"
+	"sequences"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: isprime <number>")
+		os.Exit(1)
+	}
+
+	numberStr := os.Args[1]
+	number, ok := new(big.Int).SetString(numberStr, 10)
+	if !ok {
+		fmt.Printf("Invalid number: %s\n", numberStr)
+		os.Exit(1)
+	}
+
+	if sequences.IsPrime(number) {
+		fmt.Printf("%s is a prime number.\n", numberStr)
+	} else {
+		fmt.Printf("%s is not a prime number.\n", numberStr)
+	}
+}
