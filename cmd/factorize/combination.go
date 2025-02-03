f1e4c62e04c5c51aad1207388e522a32a272eefc
cmbsolver <cmbsolver@gmail.com>
2025-02-02 21:29:51 -0600

    Adding program to generate a large bit number.  Also adding the ability of the factorizer to find prime factors that cause the product.

--- cmd/factorize/combination.go
+++ cmd/factorize/combination.go
@@ -14,9 +14,15 @@ func findCombos(db *pgx.Conn, mainId string, n *big.Int) bool {
 	number := new(big.Int).Set(n)
 	zero := big.NewInt(0)
 	seqNumber := int64(0)
+	loopCounter := int64(0)
 
 	for prime := range sequences.YieldPrimesDesc(n) {
-		fmt.Printf("Current prime: %s\n", prime.String())
+		loopCounter++
+
+		// Display the current prime based on the loop count
+		if (prime.Cmp(big.NewInt(100000000)) == -1 && loopCounter%1000000 == 0) || loopCounter%1000000000 == 0 {
+			fmt.Printf("Current prime at loop %d: %s\n", loopCounter, prime.String())
+		}
 
 		if new(big.Int).Mod(number, prime).Cmp(zero) == 0 {
 			q := new(big.Int).Div(number, prime)
