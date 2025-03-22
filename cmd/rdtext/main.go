928e766c0836384f35c0a37d59222b3414e39f8c
cmbsolver <cmbsolver@gmail.com>
2025-03-22 18:46:33 -0500

    Super cribbing from Rune Donkey output.

---
+++ cmd/rdtext/main.go
@@ -0,0 +1,279 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/jdkato/prose/v2"
+	"log"
+	"os"
+	"strings"
+
+	"github.com/xuri/excelize/v2"
+)
+
+type ColInformation struct {
+	ColName   string
+	RowCounts int
+}
+
+func main() {
+	// Define command-line flags
+	inputFile := flag.String("input", "", "Path to the input Excel file")
+	outputFile := flag.String("output", "", "Path to the output file")
+	sheetName := "Worksheet"
+
+	// Parse the flags
+	flag.Parse()
+
+	// Check if the input file is provided
+	if *inputFile == "" {
+		log.Fatalf("Input file is required")
+	}
+
+	// Open the Excel file
+	f, err := excelize.OpenFile(*inputFile)
+	if err != nil {
+		log.Fatalf("Failed to open the Excel file: %v", err)
+	}
+	defer func(f *excelize.File) {
+		err := f.Close()
+		if err != nil {
+			log.Fatalf("Failed to close the Excel file: %v", err)
+		}
+	}(f)
+
+	colInfo, err := getColInformation(f, sheetName)
+	if err != nil {
+		fmt.Printf("Failed to get column info: %v", err)
+		return
+	}
+
+	// Print the column information
+	fmt.Printf("%v\n", colInfo)
+
+	// Initialize a strings.Builder
+	var builder strings.Builder
+
+	// Call permuteCols with the provided output file name
+	err = permuteCols(f, *outputFile, sheetName, colInfo, builder, 0)
+	if err != nil {
+		fmt.Printf("Failed to permute cols: %v", err)
+	}
+}
+
+func permuteCols(f *excelize.File, outputName, sheetName string, cols []ColInformation, builder strings.Builder, currentColIdx int) (err error) {
+	localBuilder := cloneStringBuilder(&builder)
+	for i := 3; i < cols[currentColIdx].RowCounts+3; i++ {
+		if currentColIdx < (len(cols) - 1) {
+			localBuilder = cloneStringBuilder(&builder)
+			cellValue, cellError := f.GetCellValue(sheetName, fmt.Sprintf("%s%d", cols[currentColIdx].ColName, i))
+			if cellError != nil {
+				return fmt.Errorf("Failed to get cell value: %v\n", cellError)
+			}
+
+			spacer := " "
+			if currentColIdx == 0 {
+				spacer = ""
+			}
+
+			localBuilder.WriteString(spacer + cellValue)
+
+			// Write the builder content to the console
+			fmt.Printf("%d %s\n", currentColIdx, localBuilder.String())
+
+			permuteErr := permuteCols(f, outputName, sheetName, cols, *localBuilder, currentColIdx+1)
+			if permuteErr != nil {
+				return fmt.Errorf("Failed to permute columns: %v\n", permuteErr)
+			}
+		} else {
+			cellValue, cellError := f.GetCellValue(sheetName, fmt.Sprintf("%s%d", cols[currentColIdx].ColName, i))
+			if cellError != nil {
+				return fmt.Errorf("Failed to get cell value: %v\n", cellError)
+			}
+
+			localBuilder = cloneStringBuilder(&builder)
+
+			localBuilder.WriteString(" " + cellValue)
+
+			// Write the builder content to the console
+			fmt.Printf("%d %s\n", currentColIdx, localBuilder.String())
+
+			posCounts, totalWords := analyzeText(localBuilder.String())
+			probability := calculateSentenceProbability(posCounts, totalWords)
+
+			fmt.Printf("POS Counts: %+v\n", posCounts)
+			fmt.Printf("Total Words: %d\n", totalWords)
+
+			if probability > 0 {
+				fmt.Printf("Sentence Probability: %.2f%%\n", probability)
+
+				// Write the content to the output file
+				outputBytes := []byte(localBuilder.String() + "\n\n")
+				file, openError := os.OpenFile(outputName, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0644)
+				if openError != nil {
+					fmt.Printf("Failed to close file: %v\n", openError)
+					return err
+				}
+
+				_, writeErr := file.Write(outputBytes)
+				if writeErr != nil {
+					fmt.Printf("Failed to close file: %v\n", writeErr)
+					return writeErr
+				}
+
+				closeError := file.Close()
+				if closeError != nil {
+					fmt.Printf("Failed to close file: %v\n", closeError)
+					return writeErr
+				}
+			}
+		}
+	}
+
+	return nil
+}
+
+func analyzeText(text string) (map[string]int, int) {
+	doc, err := prose.NewDocument(text)
+	if err != nil {
+		log.Fatalf("Failed to create document: %v", err)
+	}
+
+	posCounts := map[string]int{
+		"Noun":        0,
+		"Verb":        0,
+		"Adjective":   0,
+		"Adverb":      0,
+		"Determiner":  0,
+		"Conjunction": 0,
+		"Preposition": 0,
+		"Pronoun":     0,
+		"Punctuation": 0,
+		"NamedEntity": 0,
+	}
+	totalWords := 0
+
+	for _, tok := range doc.Tokens() {
+		switch tok.Tag {
+		case "NN", "NNS", "NNP", "NNPS":
+			posCounts["Noun"]++
+		case "VB", "VBD", "VBG", "VBN", "VBP", "VBZ":
+			posCounts["Verb"]++
+		case "JJ", "JJR", "JJS":
+			posCounts["Adjective"]++
+		case "RB", "RBR", "RBS":
+			posCounts["Adverb"]++
+		case "DT":
+			posCounts["Determiner"]++
+		case "CC":
+			posCounts["Conjunction"]++
+		case "IN":
+			posCounts["Preposition"]++
+		case "PRP", "PRP$", "WP", "WP$":
+			posCounts["Pronoun"]++
+		case ".", ",", ":", ";", "!", "?":
+			posCounts["Punctuation"]++
+		}
+		totalWords++
+	}
+
+	posCounts["NamedEntity"] = len(doc.Entities())
+
+	return posCounts, totalWords
+}
+
+func calculateSentenceProbability(posCounts map[string]int, totalWords int) float64 {
+	if totalWords == 0 {
+		return 0.0
+	}
+
+	probability := 0.0
+	if posCounts["Noun"] > 0 && posCounts["Verb"] > 0 {
+		probability = 50.0
+		if posCounts["Adjective"] > 0 {
+			probability += 10.0
+		}
+		if posCounts["Adverb"] > 0 {
+			probability += 10.0
+		}
+		if posCounts["Determiner"] > 0 {
+			probability += 5.0
+		}
+		if posCounts["Conjunction"] > 0 {
+			probability += 5.0
+		}
+		if posCounts["Preposition"] > 0 {
+			probability += 5.0
+		}
+		if posCounts["Pronoun"] > 0 {
+			probability += 5.0
+		}
+		if posCounts["Punctuation"] > 0 {
+			probability += 10.0
+		}
+		if posCounts["NamedEntity"] > 0 {
+			probability += 5.0
+		}
+	}
+
+	return probability
+}
+
+// cloneStringBuilder clones the given strings.Builder and returns a new instance with the same content.
+func cloneStringBuilder(sb *strings.Builder) *strings.Builder {
+	// Create a new strings.Builder
+	newSb := &strings.Builder{}
+	// Write the content of the original builder to the new builder
+	newSb.WriteString(sb.String())
+	return newSb
+}
+
+// getColInformation gets the column information from the given Excel file and sheet name.
+func getColInformation(f *excelize.File, sheetName string) (cols []ColInformation, err error) {
+	// Get the column information
+	cols = make([]ColInformation, 0)
+
+	// Get all the columns in the sheet
+	columns, err := f.GetCols(sheetName)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to get columns: %v\n", err)
+	}
+
+	for colId, colValues := range columns {
+		// Get the row count
+		rowCounts := len(colValues)
+		colName := intToExcelColumn(colId + 1)
+		colInfo := ColInformation{
+			ColName:   colName,
+			RowCounts: 0,
+		}
+
+		for i := 3; i < rowCounts+5; i++ {
+			cellValue, cellError := f.GetCellValue(sheetName, fmt.Sprintf("%s%d", colName, i))
+			if cellError != nil {
+				return nil, fmt.Errorf("Failed to get cell value: %v\n", cellError)
+			}
+
+			if cellValue != "" {
+				colInfo.RowCounts++
+			} else {
+				break
+			}
+		}
+
+		cols = append(cols, colInfo)
+	}
+
+	return cols, nil
+}
+
+// intToExcelColumn converts the given integer to an Excel column name.
+func intToExcelColumn(n int) string {
+	column := ""
+	for n > 0 {
+		n-- // Adjust for 1-based indexing
+		column = string('A'+(n%26)) + column
+		n /= 26
+	}
+	return column
+}
