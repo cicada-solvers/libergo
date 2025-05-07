42da9bfcdd999475da7e80931687e830ea631cd4
cmbsolver <cmbsolver@gmail.com>
2025-05-07 17:21:39 -0500

    Removing collision prone code.

--- cmd/ipaddressgen/main.go
+++ cmd/ipaddressgen/main.go
@@ -5,10 +5,12 @@ import (
 	"net"
 	"os"
 	"strings"
+	"sync"
 )
 
+const maxFileSize = 1 << 30 // 1GB in bytes
+
 func main() {
-	// Define IPv4 ranges for classes A, B, C, D, and E
 	ranges := map[string][2]string{
 		"A": {"1.0.0.0", "127.0.0.0"},
 		"B": {"128.0.0.0", "191.255.255.255"},
@@ -17,7 +19,6 @@ func main() {
 		"E": {"240.0.0.0", "255.255.255.255"},
 	}
 
-	// Prompt user for input
 	var class string
 	fmt.Println("Enter the IPv4 class range (A, B, C, D, E):")
 	_, scanErr := fmt.Scanln(&class)
@@ -26,132 +27,145 @@ func main() {
 	}
 	class = strings.ToUpper(class)
 
-	// Validate input
 	if _, exists := ranges[class]; !exists {
 		fmt.Println("Invalid class. Please enter A, B, C, D, or E.")
 		return
 	}
 
-	// Get the range for the selected class
 	startIP := net.ParseIP(ranges[class][0]).To4()
 	endIP := net.ParseIP(ranges[class][1]).To4()
 
-	// Calculate the total number of IPs in the range
 	totalIPs := ipToInt(endIP) - ipToInt(startIP) + 1
 	ipsPerFile := totalIPs / 8
 
-	// Split the range and write to 8 files
 	for i := 0; i < 8; i++ {
-		file, failed := createFile(class, "ips", i)
-		if failed {
-			return
-		}
-
-		// Write IPs to the file
 		start := ipToInt(startIP) + int64(i)*ipsPerFile
 		end := start + ipsPerFile - 1
-		if i == 7 { // Ensure the last file includes any remaining IPs
+		if i == 7 {
 			end = ipToInt(endIP)
 		}
 
-		// Just IPs in the range
-		for ip := start; ip <= end; ip++ {
-			_, err := file.WriteString(intToIP(ip).String() + "\n")
-			if err != nil {
-				fmt.Println("Error writing to file:", err)
-				return
-			}
-		}
+		var wg sync.WaitGroup
 
-		closeFile(file)
-		file, failed = createFile(class, "ipswport", i)
-		if failed {
-			return
-		}
+		wg.Add(4) // Add the number of goroutines to the WaitGroup
 
-		// IPs with port numbers
-		for ip := start; ip <= end; ip++ {
-			for port := 1; port <= 65535; port++ {
-				ipWithPort := fmt.Sprintf("%s:%d\n", intToIP(ip).String(), port)
-				_, err := file.WriteString(ipWithPort)
-				if err != nil {
-					fmt.Println("Error writing to file:", err)
-					return
-				}
-			}
-		}
+		go func() {
+			defer wg.Done()
+			writeIPsToFile(class, "ips", start, end, false, false, i)
+		}()
 
-		closeFile(file)
-		file, failed = createFile(class, "ipswscheme", i)
-		if failed {
-			return
-		}
+		go func() {
+			defer wg.Done()
+			writeIPsToFile(class, "ipswport", start, end, true, false, i)
+		}()
 
-		// Just IPs in the range with schemes
-		for ip := start; ip <= end; ip++ {
-			for _, scheme := range getSchemes() {
-				ipWithScheme := fmt.Sprintf("%s://%s\n", scheme, intToIP(ip).String())
-				_, err := file.WriteString(ipWithScheme)
-				if err != nil {
-					fmt.Println("Error writing to file:", err)
-					return
-				}
+		go func() {
+			defer wg.Done()
+			writeIPsToFile(class, "ipswscheme", start, end, false, true, i)
+		}()
 
-				ipWithSchemeEnd := fmt.Sprintf("%s://%s/\n", scheme, intToIP(ip).String())
-				_, err = file.WriteString(ipWithSchemeEnd)
-				if err != nil {
-					fmt.Println("Error writing to file:", err)
-					return
-				}
-			}
-		}
+		go func() {
+			defer wg.Done()
+			writeIPsToFile(class, "ipswportwscheme", start, end, true, true, i)
+		}()
 
-		closeFile(file)
-		file, failed = createFile(class, "ipswportwscheme", i)
-		if failed {
-			return
-		}
+		wg.Wait() // Wait for all goroutines to finish
 
-		// IPs with port numbers with schemes
-		for ip := start; ip <= end; ip++ {
-			for _, scheme := range getSchemes() {
-				for port := 1; port <= 65535; port++ {
-					ipWithPortWithScheme := fmt.Sprintf("%s://%s:%d\n", scheme, intToIP(ip).String(), port)
-					_, err := file.WriteString(ipWithPortWithScheme)
-					if err != nil {
-						fmt.Println("Error writing to file:", err)
-						return
-					}
-
-					ipWithPortWithSchemeEnd := fmt.Sprintf("%s://%s:%d/\n", scheme, intToIP(ip).String(), port)
-					_, err = file.WriteString(ipWithPortWithSchemeEnd)
-					if err != nil {
-						fmt.Println("Error writing to file:", err)
-						return
-					}
-				}
-			}
-		}
-
-		closeFile(file)
-
-		fmt.Print("Files written successfully.\n")
+		fmt.Println("Files written successfully.")
 	}
+	fmt.Println("Files written successfully.")
+}
+
+func writeIPsToFile(class, portion string, start, end int64, includePorts, includeSchemes bool, index int) {
+	fileIndex := 1
+	file, size, failed := createFile(class, portion, index, fileIndex)
+	if failed {
+		return
+	}
+
+	for ip := start; ip <= end; ip++ {
+		if includePorts {
+			for port := 1; port <= 65535; port++ {
+				line := fmt.Sprintf("%s:%d\n", intToIP(ip).String(), port)
+				if includeSchemes {
+					for _, scheme := range getSchemes() {
+						line = fmt.Sprintf("%s://%s:%d\n", scheme, intToIP(ip).String(), port)
+						size = writeLineToFile(file, line, size)
+						if size > maxFileSize {
+							closeFile(file)
+							fileIndex++
+							file, size, failed = createFile(class, portion, index, fileIndex)
+							if failed {
+								return
+							}
+						}
+					}
+				} else {
+					size = writeLineToFile(file, line, size)
+					if size > maxFileSize {
+						closeFile(file)
+						fileIndex++
+						file, size, failed = createFile(class, portion, index, fileIndex)
+						if failed {
+							return
+						}
+					}
+				}
+			}
+		} else {
+			line := intToIP(ip).String() + "\n"
+			if includeSchemes {
+				for _, scheme := range getSchemes() {
+					line = fmt.Sprintf("%s://%s\n", scheme, intToIP(ip).String())
+					size = writeLineToFile(file, line, size)
+					if size > maxFileSize {
+						closeFile(file)
+						fileIndex++
+						file, size, failed = createFile(class, portion, index, fileIndex)
+						if failed {
+							return
+						}
+					}
+				}
+			} else {
+				size = writeLineToFile(file, line, size)
+				if size > maxFileSize {
+					closeFile(file)
+					fileIndex++
+					file, size, failed = createFile(class, portion, index, fileIndex)
+					if failed {
+						return
+					}
+				}
+			}
+		}
+	}
+	closeFile(file)
+}
+
+func writeLineToFile(file *os.File, line string, size int64) int64 {
+	lineSize := int64(len(line))
+	_, err := file.WriteString(line)
+	if err != nil {
+		fmt.Println("Error writing to file:", err)
+		return size
+	}
+	return size + lineSize
+}
+
+func createFile(class, portion string, index, fileIndex int) (*os.File, int64, bool) {
+	fileName := fmt.Sprintf("%s_%s_part_%d_%d.txt", class, portion, index, fileIndex)
+	file, err := os.Create(fileName)
+	if err != nil {
+		fmt.Println("Error creating file:", err)
+		return nil, 0, true
+	}
+	return file, 0, false
 }
 
 func closeFile(file *os.File) {
-	closeError := file.Close()
-	if closeError != nil {
-		fmt.Println("Error closing file:", closeError)
+	err := file.Close()
+	if err != nil {
+		fmt.Println("Error closing file:", err)
 	}
-}
-
-func createFile(class, portion string, i int) (*os.File, bool) {
-	fileName := fmt.Sprintf("%s_%s_range_part_%d.txt", class, portion, i+1)
-	file, createError := os.Create(fileName)
-	if createError != nil {
-		fmt.Println("Error creating file:", createError)
-		return nil, true
-	}
-	return file, false
 }
