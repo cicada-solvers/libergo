32f14781ad4c7bb4c7e1263111b7ab3739a1df10
cmbsolver <cmbsolver@gmail.com>
2025-06-29 21:46:24 -0500

    Added program to index Goldbach addends to the database.

--- cmd/invgoldbach/main.go
+++ cmd/invgoldbach/main.go
@@ -5,6 +5,8 @@ import (
 	"gorm.io/gorm"
 	"liberdatabase"
 	"numeric"
+	"runtime"
+	"sync"
 )
 
 func main() {
@@ -18,29 +20,55 @@ func main() {
 		}
 	}(conn)
 
-	largestNumber := int64(2147483647) + int64(2147483647)
+	// Create a channel for numbers to be processed
+	numberChannel := make(chan int64)
 
-	for i := int64(4); i <= largestNumber; i++ {
-		if numeric.IsNumberEven(i) {
-			gbp := numeric.NewGoldbachPairs()
-			primeList := liberdatabase.GetPrimeListLessThanValue(conn, i)
+	// Determine the number of workers (CPU count × 2)
+	numWorkers := runtime.NumCPU() * 2
+	fmt.Printf("Using %d worker goroutines\n", numWorkers)
 
-			solveError := gbp.SolveForNumber(i, &primeList)
-			if solveError != nil {
-				fmt.Println("Error solving for number:", i, solveError)
-				fmt.Println("----------------------------------------")
-			} else {
-				pairCount := len(gbp.GetGoldbachPairs())
-				fmt.Println("Number:", i, pairCount)
-				fmt.Println("----------------------------------------")
+	// Use WaitGroup to wait for all workers to finish
+	var wg sync.WaitGroup
 
-				// Now we need to add them to the database
-				goldbachNumber := liberdatabase.AddGoldbachNumber(conn, i, true)
-				for _, pair := range gbp.GetGoldbachPairs() {
-					liberdatabase.AddGoldbachAddend(conn, goldbachNumber.Id, pair.AddendOne, pair.AddendTwo)
+	for i := 0; i < numWorkers; i++ {
+		wg.Add(1)
+		go func(workerID int) {
+			for num := range numberChannel {
+				gbp := numeric.NewGoldbachPairs()
+				primeList := liberdatabase.GetPrimeListLessThanValue(conn, num)
+
+				solveError := gbp.SolveForNumber(num, &primeList)
+				if solveError != nil {
+					fmt.Println("Error solving for number:", num, solveError)
+					fmt.Println("----------------------------------------")
+				} else {
+					pairCount := len(gbp.GetGoldbachPairs())
+					fmt.Println("Number:", num, pairCount)
+					fmt.Println("----------------------------------------")
+
+					// Now we need to add them to the database
+					goldbachNumber := liberdatabase.AddGoldbachNumber(conn, num, true)
+					for _, pair := range gbp.GetGoldbachPairs() {
+						liberdatabase.AddGoldbachAddend(conn, goldbachNumber.Id, pair.AddendOne, pair.AddendTwo)
+					}
 				}
 			}
-
-		}
+		}(i)
 	}
+
+	go func() {
+		largestNumber := int64(2147483647) + int64(2147483647)
+		for i := int64(4); i <= largestNumber; i++ {
+			if numeric.IsNumberEven(i) {
+				numberChannel <- i
+			}
+		}
+		// Close the channel when done generating
+		close(numberChannel)
+		fmt.Println("Number generation completed")
+	}()
+
+	// Wait for all workers to finish
+	wg.Wait()
+	fmt.Println("All workers have completed")
 }
